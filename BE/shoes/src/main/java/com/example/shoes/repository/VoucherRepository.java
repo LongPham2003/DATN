bb02f2d9d02d172e4c0677175bc8d4a5b0c827a3
pham thanh long <phamlong20031@gmail.com>
2024-09-23 14:17:12 +0700

    done

--- BE/shoes/src/main/java/com/example/shoes/repository/VoucherRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.shoes.repository;
-
-import com.example.shoes.entity.Voucher;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface VoucherRepository extends JpaRepository<Voucher,Integer> {
-}
