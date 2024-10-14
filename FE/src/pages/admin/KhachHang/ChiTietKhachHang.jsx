ac53eeb5082ccfb0450dc1c1006274bfa668ab85
pham thanh long <phamlong20031@gmail.com>
2024-10-14 12:36:14 +0700

    fix detail  kh

--- FE/src/pages/admin/KhachHang/ChiTietKhachHang.jsx
+++ FE/src/pages/admin/KhachHang/ChiTietKhachHang.jsx
@@ -24,10 +24,7 @@ export default function ChiTietKhachHang() {
     ngaySinh: "",
     gioiTinh: "",
     diaChi: "",
-<<<<<<< HEAD
     soNhaDuongThonXom: "",
-=======
->>>>>>> origin/master
     province: "",
     district: "",
     ward: "",
@@ -63,7 +60,7 @@ export default function ChiTietKhachHang() {
         province: selectedProvinceData.name,
         district: "",
         ward: "",
-        diaChi: "", // Reset địa chỉ khi thay đổi tỉnh
+        soNhaDuongThonXom: "", // Reset địa chỉ khi thay đổi tỉnh
       }));
     } else {
       setError("Không tìm thấy tỉnh/thành phố.");
@@ -122,7 +119,6 @@ export default function ChiTietKhachHang() {
           `http://localhost:8080/khachhang/${id}`,
         );
         const customerData = response.data.result;
-<<<<<<< HEAD
 
         // Lấy thông tin địa chỉ từ phần tử đầu tiên trong mảng diaChis
         const addressData = customerData.diaChis[0];
@@ -138,8 +134,6 @@ export default function ChiTietKhachHang() {
         );
 
         // Cập nhật dữ liệu vào form
-=======
->>>>>>> origin/master
         setFormData({
           hoTen: customerData.hoTen,
           email: customerData.email,
@@ -147,12 +141,11 @@ export default function ChiTietKhachHang() {
           ngaySinh: customerData.ngaySinh,
           gioiTinh: customerData.gioiTinh,
           trangThai: customerData.trangThai,
-<<<<<<< HEAD
           province: addressData.tinhThanhPho,
           district: addressData.huyenQuan,
           ward: addressData.xaPhuong,
           soNhaDuongThonXom: addressData.soNhaDuongThonXom,
-          diaChi: addressData.diaChiChiTiet,
+          diaChiChiTiet: addressData.diaChiChiTiet,
         });
 
         // Cập nhật các giá trị cho dropdown
@@ -160,44 +153,36 @@ export default function ChiTietKhachHang() {
         setSelectedDistrict(districtData ? districtData.idDistrict : "");
         setSelectedCommune(communeData ? communeData.idCommune : "");
         setFullAddress(addressData.diaChiChiTiet);
-=======
-          province: customerData.tinhThanhPho,
-          district: customerData.huyenQuan,
-          ward: customerData.xaPhuong,
-          diaChi: customerData.diaChiChiTiet,
-        });
-        setFullAddress(customerData.diaChiChiTiet);
->>>>>>> origin/master
       } catch (error) {
         setError("Không tìm thấy thông tin khách hàng.");
         console.error("Error fetching customer data", error);
       }
     };
-<<<<<<< HEAD
 
     // Gọi fetchCustomerData mỗi khi diaChiData thay đổi
     if (diaChiData.province.length > 0) {
       fetchCustomerData();
     }
   }, [id, diaChiData]);
-=======
-    fetchCustomerData();
-  }, [id]);
->>>>>>> origin/master
 
   useEffect(() => {
     if (
-      formData.diaChi &&
+      formData.soNhaDuongThonXom &&
       formData.ward &&
       formData.district &&
       formData.province
     ) {
-      const fullAddr = `${formData.diaChi} - ${formData.ward} - ${formData.district} - ${formData.province}`;
+      const fullAddr = `${formData.soNhaDuongThonXom} - ${formData.ward} - ${formData.district} - ${formData.province}`;
       setFullAddress(fullAddr);
     } else {
       setFullAddress("");
     }
-  }, [formData.diaChi, formData.ward, formData.district, formData.province]);
+  }, [
+    formData.soNhaDuongThonXom,
+    formData.ward,
+    formData.district,
+    formData.province,
+  ]);
 
   const handleUpdate = async (e) => {
     e.preventDefault();
@@ -211,10 +196,7 @@ export default function ChiTietKhachHang() {
           ngaySinh: formData.ngaySinh,
           gioiTinh: formData.gioiTinh,
           trangThai: formData.trangThai,
-<<<<<<< HEAD
-          soNhaDuongThonXom: formData.diaChi,
-=======
->>>>>>> origin/master
+          soNhaDuongThonXom: formData.soNhaDuongThonXom,
           diaChiChiTiet: fullAddress, // Gửi địa chỉ đầy đủ
           tinhThanhPho: formData.province,
           huyenQuan: formData.district,
@@ -380,22 +362,14 @@ export default function ChiTietKhachHang() {
                     ))}
               </select>
             </div>
-<<<<<<< HEAD
             <div className="w-full p-2 sm:w-1/2">
               <label className="mb-1 block">Số Nhà, Làng :</label>
               <input
                 type="text"
                 value={formData.soNhaDuongThonXom}
-=======
-            <div className="w-full p-2">
-              <label className="mb-1 block">Số Nhà, Làng :</label>
-              <input
-                type="text"
-                value={formData.diaChi}
->>>>>>> origin/master
                 className="w-full rounded border p-2"
                 onChange={handleChange}
-                name="diaChi" // Đảm bảo có name để xử lý
+                name="soNhaDuongThonXom" // Đảm bảo có name để xử lý
               />
             </div>
             <div className="w-full p-2">
@@ -404,7 +378,8 @@ export default function ChiTietKhachHang() {
                 type="text"
                 value={fullAddress}
                 className="w-full rounded border p-2"
-                readOnly
+                onChange={fullAddress}
+                disabled
               />
             </div>
           </div>
