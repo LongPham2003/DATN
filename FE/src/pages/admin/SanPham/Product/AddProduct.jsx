f8dc0ce95f9c9029345568cfba88661dfbfd0b48
Hoanh2k <tranxuanhoanh13@gmail.com>
2024-10-15 21:53:41 +0700

    updatevalidate

--- FE/src/pages/admin/SanPham/Product/AddProduct.jsx
+++ FE/src/pages/admin/SanPham/Product/AddProduct.jsx
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { values } from "lodash";
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom"; // Import useNavigate
 import { Bounce, toast, ToastContainer } from "react-toastify";
@@ -10,16 +11,20 @@ export default function AddProduct() {
   const [loaiSelect, setLoaiSelect] = useState([]);
   const [errorTenSP, setErrorTenSP] = useState("");
   const [errorLoai, setErrorLoai] = useState("");
+  const [danhSachTenSP, setDanhSachTenSP] = useState("");
   const [trangThai] = useState(true); // Sử dụng giá trị mặc định true cho trạng thái
   const navigate = useNavigate(); // Sử dụng useNavigate để điều hướng
 
   let ApiGetAllLoai = `http://localhost:8080/api/loai/getall`;
   let ApiAddSPMoi = `http://localhost:8080/api/sanpham/add`;
+  let ApiGetAllten = `http://localhost:8080/api/sanpham/ten`;
 
   const getAllLoai = async () => {
     try {
       const data = await axios.get(ApiGetAllLoai);
+      const SP = await axios.get(ApiGetAllten);
       setLoaiSelect(data.data.result);
+      setDanhSachTenSP(SP.data);
     } catch (error) {
       console.log(error);
     }
@@ -100,6 +105,16 @@ export default function AddProduct() {
     }
   };
 
+  const handelInput = (e) => {
+    settenSanPham(e.target.value);
+    const tenDaTonTai = danhSachTenSP.includes(e.target.value);
+    if (tenDaTonTai) {
+      setErrorTenSP("Ten Da Ton Tai");
+    } else {
+      setErrorTenSP("");
+    }
+  };
+
   return (
     <div className="mx-auto mt-10 w-72">
       <h1 className="mb-5 text-2xl font-bold">Thêm sản phẩm</h1>
@@ -113,9 +128,7 @@ export default function AddProduct() {
             id="tenSanPham"
             value={tenSanPham}
             name="tenSanPham"
-            onChange={(e) => {
-              settenSanPham(e.target.value), setErrorTenSP("");
-            }}
+            onChange={handelInput}
             className="w-full rounded-md border px-3 py-2"
           />
           {errorTenSP && <p className="text-red-500">{errorTenSP}</p>}
