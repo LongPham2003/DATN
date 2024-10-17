e02a9123395da78086a0eaba017f835e74bbcfd1
Hoanh2k <tranxuanhoanh13@gmail.com>
2024-10-17 08:36:53 +0700

    fix bug

--- BE/shoes/src/main/java/com/example/shoes/repository/HinhAnhRepo.java
+++ BE/shoes/src/main/java/com/example/shoes/repository/HinhAnhRepo.java
@@ -1,7 +1,13 @@
 package com.example.shoes.repository;
 
+import com.example.shoes.dto.hinhanh.response.HinhAnhResponse;
 import com.example.shoes.entity.HinhAnh;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface HinhAnhRepo extends JpaRepository<HinhAnh, Integer> {
+
 }
