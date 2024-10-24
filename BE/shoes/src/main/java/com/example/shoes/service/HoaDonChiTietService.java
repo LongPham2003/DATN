3aa167916121002b17d9edac0d5ddf2f5be7a504
dang an hoa <dhoa05808@gmail.com>
2024-10-24 10:55:33 +0700

    hoa update hoadon ,tich hop vnpay,them api hoadonchitiet,lichsuhoadon

---
+++ BE/shoes/src/main/java/com/example/shoes/service/HoaDonChiTietService.java
@@ -0,0 +1,9 @@
+package com.example.shoes.service;
+
+import com.example.shoes.dto.hoadonchitiet.response.HoaDonChiTietResponse;
+
+import java.util.List;
+
+public interface HoaDonChiTietService {
+    List<HoaDonChiTietResponse> getAllHoaDonCT();
+}
--- BE/shoes/src/main/java/com/example/shoes/service/HoaDonService.java
+++ BE/shoes/src/main/java/com/example/shoes/service/HoaDonService.java
@@ -1,15 +1,12 @@
 package com.example.shoes.service;
 
-import com.example.shoes.dto.hoadon.request.HoaDonRequest;
 import com.example.shoes.dto.hoadon.response.HoaDonResponse;
 import com.example.shoes.dto.hoadonchitiet.request.HoaDonChiTietRequest;
 import com.example.shoes.dto.phuongthucthanhtoan.request.PhuongThucThanhToanRequest;
-
-
 import java.util.List;
 
 public interface HoaDonService {
- HoaDonResponse createHoaDon(HoaDonRequest hoaDonRequest);
+ HoaDonResponse createHoaDon();
  HoaDonResponse updateHoaDon( Integer id,HoaDonChiTietRequest chiTietRequest);
  HoaDonResponse findByid(Integer id);
  HoaDonResponse deleteHoaDon(Integer idHoaDon);
@@ -17,4 +14,6 @@ public interface HoaDonService {
  void thanhToan(Integer idHoaDon, PhuongThucThanhToanRequest phuongThucThanhToanRequest);
  HoaDonResponse addSanPhamChiTietToHoaDon(Integer idHoaDon, HoaDonChiTietRequest chiTietRequest);
  void apPhieuGiamGiaHoaDon(Integer idHoaDon, Integer idPhieuGiamGia);
+ List<HoaDonResponse> getAllTrangThaiTrue();
+ List<HoaDonResponse> getAllTrangThaiFalse();
 }
