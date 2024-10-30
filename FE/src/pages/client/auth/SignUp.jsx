6701e0208dcd08d0962a44fa1a858b960820f549
pham thanh long <phamlong20031@gmail.com>
2024-10-30 16:24:17 +0700

    animate

--- FE/src/pages/client/auth/SignUp.jsx
+++ FE/src/pages/client/auth/SignUp.jsx
@@ -1,8 +1,10 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { Bounce, toast, ToastContainer } from "react-toastify";
+import Loading from "../../util/Loading.jsx";
 
 const SignUp = () => {
+  const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
   const [formData, setFormData] = useState({
     email: "",
@@ -28,6 +30,8 @@ const SignUp = () => {
       setError("Passwords do not match!");
       return;
     }
+
+    setLoading(true);
 
     try {
       // eslint-disable-next-line no-unused-vars
@@ -55,75 +59,83 @@ const SignUp = () => {
       }, 500);
     } catch (error) {
       setError(error.message);
+    } finally
+    {
+      setLoading(false);
     }
   };
   return (
     <div className="flex h-screen items-center justify-center bg-gray-100">
-      <form
-        onSubmit={handleSubmit}
-        className="w-96 rounded-lg bg-white p-8 shadow-lg"
-      >
-        <h2 className="mb-6 text-center text-2xl font-bold">
-          Đăng ký tài khoản
-        </h2>
+      {loading ? (<Loading></Loading>):
+        (
+          <form
+            onSubmit={handleSubmit}
+            className="w-96 rounded-lg bg-white p-8 shadow-lg"
+          >
+            <h2 className="mb-6 text-center text-2xl font-bold">
+              Đăng ký tài khoản
+            </h2>
 
-        <div className="mb-4">
-          <label className="block text-gray-700">UserName</label>
-          <input
-            type="text"
-            name="hoTen"
-            value={formData.hoTen}
-            onChange={handleChange}
-            className="mt-2 w-full rounded-lg border px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
-            required
-          />
-        </div>
+            <div className="mb-4">
+              <label className="block text-gray-700">UserName</label>
+              <input
+                type="text"
+                name="hoTen"
+                value={formData.hoTen}
+                onChange={handleChange}
+                className="mt-2 w-full rounded-lg border px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                required
+              />
+            </div>
 
-        <div className="mb-4">
-          <label className="block text-gray-700">Email</label>
-          <input
-            type="email"
-            name="email"
-            value={formData.email}
-            onChange={handleChange}
-            className="mt-2 w-full rounded-lg border px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
-            required
-          />
-        </div>
+            <div className="mb-4">
+              <label className="block text-gray-700">Email</label>
+              <input
+                type="email"
+                name="email"
+                value={formData.email}
+                onChange={handleChange}
+                className="mt-2 w-full rounded-lg border px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                required
+              />
+            </div>
 
-        <div className="mb-4">
-          <label className="block text-gray-700">Password</label>
-          <input
-            type="password"
-            name="password"
-            value={formData.password}
-            onChange={handleChange}
-            className="mt-2 w-full rounded-lg border px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
-            required
-          />
-        </div>
+            <div className="mb-4">
+              <label className="block text-gray-700">Password</label>
+              <input
+                type="password"
+                name="password"
+                value={formData.password}
+                onChange={handleChange}
+                className="mt-2 w-full rounded-lg border px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                required
+              />
+            </div>
 
-        <div className="mb-4">
-          <label className="block text-gray-700">Confirm Password</label>
-          <input
-            type="password"
-            name="confirmPassword"
-            value={formData.confirmPassword}
-            onChange={handleChange}
-            className="mt-2 w-full rounded-lg border px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
-            required
-          />
-        </div>
+            <div className="mb-4">
+              <label className="block text-gray-700">Confirm Password</label>
+              <input
+                type="password"
+                name="confirmPassword"
+                value={formData.confirmPassword}
+                onChange={handleChange}
+                className="mt-2 w-full rounded-lg border px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                required
+              />
+            </div>
 
-        {error && <p className="mb-4 text-sm text-red-500">{error}</p>}
+            {error && <p className="mb-4 text-sm text-red-500">{error}</p>}
 
-        <button
-          type="submit"
-          className="w-full rounded-lg bg-blue-500 py-2 text-white transition duration-200 hover:bg-blue-600"
-        >
-          Đăng ký
-        </button>
-      </form>
+            <button
+              type="submit"
+              className="w-full rounded-lg bg-blue-500 py-2 text-white transition duration-200 hover:bg-blue-600"
+            >
+              Đăng ký
+            </button>
+          </form>
+        )
+      }
+
       <ToastContainer
         position="top-right"
         autoClose={1000}
