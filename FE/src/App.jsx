d809f4a296d0f965fe353b8e786fcedec49e272d
pham thanh long <phamlong20031@gmail.com>
2024-11-11 14:54:40 +0700

    fix voucher thuộc tính ....

--- FE/src/App.jsx
+++ FE/src/App.jsx
@@ -33,20 +33,63 @@ import BanHangTaiQuay from "./pages/admin/BanHangTaiQuay/BanHangTaiQuay";
 import HomePage from "./pages/client/Home/homePage.jsx";
 import PaymentResult from "./pages/admin/BanHangTaiQuay/Payment.jsx";
 import DanhSachHoaDon from "./pages/admin/QuanLiHoaDon/DanhSachHoaDon.jsx";
+import SanPham from "./pages/client/SanPham/SanPham.jsx";
+import LienHe from "./pages/client/LienHe/LienHe.jsx";
+import TrangChu from "./pages/client/TrangChu/TrangChu.jsx";
+import GioHang from "./pages/client/GioHang/GioHang.jsx";
 
 function App() {
   return (
     <>
     <BrowserRouter>
       <Routes>
-        <Route
-          path="/"
-          element={
-            <>
-              <HomePage />
-            </>
-          }
-        />
+        <Route path="/" element={<HomePage />}>
+          <Route
+            path="SanPham"
+            element={
+              <>
+                <Helmet>
+                  <title>Sản Phẩm</title>
+                </Helmet>
+                <SanPham />
+              </>
+            }
+          />
+
+          <Route
+            path="LienHe"
+            element={
+              <>
+                <Helmet>
+                  <title>Liên Hệ</title>
+                </Helmet>
+                <LienHe />
+              </>
+            }
+          />
+          <Route
+            path=""
+            element={
+              <>
+                <Helmet>
+                  <title>Trang Chủ</title>
+                </Helmet>
+                <TrangChu />
+              </>
+            }
+          />
+          <Route
+            path="GioHang"
+            element={
+              <>
+                <Helmet>
+                  <title>Giỏ hàng</title>
+                </Helmet>
+                <GioHang />
+              </>
+            }
+          />
+        </Route>
         <Route
           path="/403"
           element={
