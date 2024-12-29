c7baa397cc3a247ad019412db7b4353dcbb29034
pham thanh long <phamlong20031@gmail.com>
2024-12-29 16:35:29 +0700

    done

--- FE/src/pages/admin/QuanLiHoaDon/ThongTinKhachHang.jsx
+++ FE/src/pages/admin/QuanLiHoaDon/ThongTinKhachHang.jsx
@@ -365,12 +365,14 @@ const ThongTinKhachHang = ({ hoaDon, fillHoaDon, fillHoaDonChiTiet }) => {
           {/* Cột phải */}
           <div className="w-1/2 space-y-4 pl-4">
             <div className="flex items-center justify-end">
-              <button
-                onClick={openModelCapNhatThongTin}
-                className="rounded bg-blue-500 px-2 text-white"
-              >
-                Cập nhật thông tin
-              </button>
+              {hoaDon.trangThaiDonHang === "Chờ Xác Nhận" && (
+                <button
+                  onClick={openModelCapNhatThongTin}
+                  className="rounded bg-blue-500 px-2 text-white"
+                >
+                  Cập nhật thông tin
+                </button>
+              )}
             </div>
             <div className="flex items-center justify-between">
               <span className="font-medium text-gray-600">
